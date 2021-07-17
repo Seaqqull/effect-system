2114b3f7bec278ac8a8a011529098dd83034ccb9
Dmytro <seaqqullprogramming@gmail.com>
2021-07-17 20:56:12 +0200

    Update: Utilities classes
     Added:
     - BaseMono
    
     Description:
     - [BaseMono]: Caches important data

---
+++ EffectSystem/Assets/Scripts/Utilities/BaseMono.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+
+namespace Effects.Utilities
+{
+    /// <summary>
+    /// Stores references to important GameObject components
+    /// </summary>
+    public class BaseMono : MonoBehaviour
+    {
+        public Transform Transform
+        {
+            get;
+            private set;
+        }
+        public Quaternion Rotation
+        {
+            get => Transform.rotation;
+            set => Transform.rotation = value;
+        }
+        public GameObject GameObj
+        {
+            get;
+            private set;
+        }
+        public Vector3 Position
+        {
+            get => Transform.position;
+        }
+        public Vector3 Forward
+        {
+            get => Transform.forward;
+        }
+
+
+        protected virtual void Awake()
+        {
+            Transform = transform;
+            GameObj = gameObject;
+        }
+    }
+}
